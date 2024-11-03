23e329cea7b8cfedc7c04aa3576510d4fb2cb794
fgdamasceno <fgdamasceno@gmail.com>
2024-11-02 23:05:53 -0300

    palindrome checker project (certification project 1)

---
+++ 08. Build a Palindrome Checker Project (Certification Project)/script.js
@@ -0,0 +1,48 @@
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const textResult = document.getElementById("result");
+
+function cleanInputString() {
+  const regex = /[a-z0-9\s]+/;
+  return textInput.value
+    .replace(/\s+/, "")
+    .toLowerCase()
+    .match(regex)
+    .toString();
+}
+
+function palindromeChecker() {
+  let reversedInput = cleanInputString().split("").reverse().join("");
+  let isPalindrome = cleanInputString() === reversedInput;
+  return isPalindrome;
+}
+
+function update() {
+  if (textInput.value === "") {
+    alert("Please input a value");
+  } else {
+    const result = palindromeChecker()
+      ? " is a palindrome."
+      : " is not a palindrome.";
+    if (palindromeChecker()) {
+      textResult.classList.remove("hidden");
+      textResult.innerHTML = `<p class="user-input"><strong>${textInput.value}</strong>${result}</p>`;
+      result;
+    } else {
+      textResult.classList.remove("hidden");
+      textResult.innerHTML = `<p class="user-input"><strong>${textInput.value}</strong>${result}</p>`;
+    }
+  }
+}
+
+checkBtn.addEventListener("click", () => {
+  update();
+  textInput.value = "";
+});
+
+textInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    update();
+    textInput.value = "";
+  }
+});
